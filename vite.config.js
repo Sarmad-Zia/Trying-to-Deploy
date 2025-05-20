16d8e6304410f8336803d407bf7f9b953608aa9e
Sarmad Zia <sarmadzia565@gmail.com>
2025-05-20 19:46:09 +0500

    trying after video

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  basename: '/Trying-to-Deploy/',
+  base: '/Trying-to-Deploy/',
   plugins: [react(),tailwindcss()],
 })
